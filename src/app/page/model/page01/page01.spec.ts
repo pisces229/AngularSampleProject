3a49fd451b7130bf412ea17185a2199c2d54d89e
DESKTOP-4PRC3LQ\pisces <pisces72297@gmail.com>
2021-05-22 17:28:06 +0800

    no message

--- src/app/page/model/page01/page01.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Page01 } from './page01';
-
-describe('Page01', () => {
-  it('should create an instance', () => {
-    expect(new Page01()).toBeTruthy();
-  });
-});
--- src/app/page/model/page02/page02.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Page02 } from './page02';
-
-describe('Page02', () => {
-  it('should create an instance', () => {
-    expect(new Page02()).toBeTruthy();
-  });
-});
